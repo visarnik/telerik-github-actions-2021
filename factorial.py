ae5511f8a60e11e502504a77dfc5df259515733d
visarnik <visarnik@gmail.com>
2021-10-25 20:36:32 +0300

    just fix

--- factorial.py
+++ factorial.py
@@ -9,14 +9,12 @@ def factorial(n):
         factorial = factorial * i
     return factorial
 
-
 def sum_factorial():
     for i in range(50):
         final_list.append(factorial(i))
     result = sum(final_list)
     print("Final SUM = {}".format(result))
     return result
-
 
 if __name__ == "__main__":
     sum_factorial()
